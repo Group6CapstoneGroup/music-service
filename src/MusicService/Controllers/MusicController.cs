d138784cd9138506e08652d4ffabdd757f0ff4fc
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2021-11-05 09:32:29 -0700

    Adding inital solution and project to music service

---
+++ src/MusicService/Controllers/MusicController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using MusicService.Models;
+using MusicService.Common;
+using MusicService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicService.Controllers
+{
+    [ApiController]
+    [Route("/api/Music")]
+    public class MusicController : Controller
+    {
+        private readonly IMusicService _service;
+        private readonly IMapper _mapper;
+        private readonly IStringLocalizer<Music> _localizer;
+
+        [HttpGet("")]
+        [ProducesResponseType(typeof(IEnumerable<Music>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Error), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Error), StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetCostDivisions()
+        {
+            var result = _service.GetAsync();
+
+            if (result is null)
+            {
+                return NoContent();
+            }
+
+            var musicList = new List<Music>();
+            await foreach (var music in result)
+            {
+               musicList.Add(_mapper.Map<Music>(music));
+            }
+
+            return Ok(ServiceResponse.Successful(musicList));
+        }
+
+    }
+}
---
+++ src/MusicService/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicService.Models
+{
+    public class Music
+    {
+        public Guid RecordId { get; set; }
+        public string Track { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+    }
+}
